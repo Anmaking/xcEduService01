5b3678dd6c73ffe0ac9b788bf55081ee14fa1345
anan_global <1145257602@qq.com>
2020-01-18 21:33:48 +0800

    第5天代码提交

---
+++ test-rabbitmq-producer/src/main/java/com/xuecheng/test/rabbitmq/TestRabbitmqApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng.test.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * describe:
+ *
+ * @author AnAn
+ * @date 2020/01/18
+ */
+@SpringBootApplication
+public class TestRabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRabbitmqApplication.class,args);
+    }
+}
