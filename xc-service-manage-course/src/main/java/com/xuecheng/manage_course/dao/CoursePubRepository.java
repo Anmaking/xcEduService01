dc6d3ee066666ed9abdc0003c3694acf37f9e2f3
anan_global <1145257602@qq.com>
2020-02-05 22:49:44 +0800

    第11天代码提交

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CoursePubRepository.java
@@ -0,0 +1,13 @@
+package com.xuecheng.manage_course.dao;
+
+import com.xuecheng.framework.domain.course.CoursePub;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * describe:
+ *
+ * @author AnAn
+ * @date 2020/02/05
+ */
+public interface CoursePubRepository extends JpaRepository<CoursePub,String> {
+}
