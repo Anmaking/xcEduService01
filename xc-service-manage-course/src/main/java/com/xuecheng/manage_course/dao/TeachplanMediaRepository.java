4d6e24a2a361a3b8856dc95af251f34990a299f4
anan_global <1145257602@qq.com>
2020-02-10 21:12:53 +0800

    第14天代码提交

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/TeachplanMediaRepository.java
@@ -0,0 +1,13 @@
+package com.xuecheng.manage_course.dao;
+
+import com.xuecheng.framework.domain.course.TeachplanMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * describe:
+ *
+ * @author AnAn
+ * @date 2020/02/10
+ */
+public interface TeachplanMediaRepository extends JpaRepository<TeachplanMedia,String> {
+}
